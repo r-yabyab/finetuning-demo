5d56a06faf521b474d186bbe7bdc9d9094237da4
Scruffy pc <cayabyabrr@yahoo.com>
2025-07-18 14:21:34 -0500

    gitignore

---
+++ src/verify.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('./data/data.txt')
+
+print(df.head())
+
+df_train=df.sample(frac=0.95,random_state=200)
+df_eval=df.drop(df_train.index)
+
+df_train.to_json("./data/data_chunk_train.jsonl", orient="records", lines=True)
+df_eval.to_json("./data/data_chunk_eval.jsonl", orient="records", lines=True)
