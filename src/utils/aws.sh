e1fdc3d08cbf9e619cbbee38b740ddf3176d313c
Scruffy pc <cayabyabrr@yahoo.com>
2025-07-20 22:28:00 -0500

    OOM torch log with 16gb vram

--- src/utils/aws.sh
+++ src/utils/aws.sh
@@ -17,18 +17,6 @@ pip install torch==2.2  # takes like 5 minutes on aws
 pip install -r requirements.txt
 pip install mistral-common==1.5.0 # downgrade needed
 
-# check if has nvidia drivers
-# dl deps first then this or else wheels for xformers gets stuck
-# https://documentation.ubuntu.com/server/how-to/graphics/install-nvidia-drivers/index.html
-    # sudo apt-get install -y ubuntu-drivers-common
-    # sudo ubuntu-drivers list --gpgpu
-    # sudo ubuntu-drivers install --gpgpu nvidia:535-server
-    # sudo apt install nvidia-cuda-toolkit
-    # nvcc --version
-    # sudo apt install nvidia-utils-535-server
-    # sudo reboot # reboot to apply changes
-    # nvidia-smi
-
 # download mistral 7B model
 # takes like 10 minutes to dl
 cd
@@ -58,9 +46,27 @@ python yaml-create.py
 
 sudo vim example.yaml # Change seq_len to like 512 depending on data size
 mv example.yaml ../../mistral-finetune/
+cd $HOME/mistral-finetune
 
-#set up cuda drivers
-sudo apt-get install -y ubuntu-drivers-common
+pip install "numpy<2.0"
+
+# check if has nvidia drivers
+# dl deps first then this or else wheels for xformers gets stuck
+# https://documentation.ubuntu.com/server/how-to/graphics/install-nvidia-drivers/index.html
+    # sudo apt-get install -y ubuntu-drivers-common
+    # sudo ubuntu-drivers list --gpgpu
+    # sudo ubuntu-drivers install --gpgpu nvidia:535-server
+    # sudo apt install nvidia-cuda-toolkit # skipped this
+    # nvcc --version    # and this skipped
+    # sudo apt install nvidia-utils-535-server
+    # sudo reboot # reboot to apply changes, best if you rebooted from console
+    # nvidia-smi
+
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export CUDA_VISIBLE_DEVICES=0
 
 # start training
 torchrun --nproc-per-node 1 -m train example.yaml
+
+# 16gb vram g4dn.xlarge gives this OOM error when run torch:
+# torch.cuda.OutOfMemoryError: CUDA out of memory. Tried to allocate 514.00 MiB. GPU 0 has a total capacity of 14.58 GiB of which 479.56 MiB is free. Including non-PyTorch memory, this process has 14.11 GiB memory in use. Of the allocated memory 13.58 GiB is allocated by PyTorch, and 115.49 MiB is reserved by PyTorch but unallocated. If reserved but unallocated memory is large try setting PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True to avoid fragmentation.  See documentation for Memory Management  (https://pytorch.org/docs/stable/notes/cuda.html#environment-variables)
