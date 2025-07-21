df7d5e339659c31bd8a07e4254655b9fbe9ef5e0
Scruffy pc <cayabyabrr@yahoo.com>
2025-07-21 17:31:26 -0500

    Unsloth script works

--- src/utils/aws.sh
+++ src/utils/aws.sh
@@ -13,16 +13,16 @@ conda activate myenv
 # requirements
 cd $HOME && git clone https://github.com/mistralai/mistral-finetune.git
 cd mistral-finetune
-pip install torch==2.2  # takes like 5 minutes on aws
-pip install -r requirements.txt
+pip install torch==2.2  
+pip install -r requirements.txt # takes like 5 minutes on aws
 pip install mistral-common==1.5.0 # downgrade needed
 
 # download mistral 7B model
 # takes like 10 minutes to dl
 cd
 mkdir -p ~/${HOME}/mistral_models
-cd ${HOME} && wget https://models.mistralcdn.com/mistral-7b-v0-3/mistral-7B-v0.3.tar
 mkdir mistral_models
+cd ${HOME} && wget https://models.mistralcdn.com/mistral-7b-v0-3/mistral-7B-v0.3.tar
 tar -xf mistral-7B-v0.3.tar -C mistral_models
 sudo rm -r mistral-7B-v0.3.tar
 
@@ -32,8 +32,12 @@ cd finetuning-demo/src/
 mkdir data
 cd data
 touch data_chunk_train.jsonl data_chunk_eval.jsonl # fill data, watch out for empty line at end of file
+sudo vim data_chunk_train.jsonl
+sudo vim data_chunk_eval.jsonl
 
-# data prep
+# validate data and config yaml
+cd $HOME/mistral-finetune/example
+sudo vim 7B.yaml # put data
 cd $HOME/mistral-finetune
 python -m utils.reformat_data ../finetuning-demo/src/data/data_chunk_train.jsonl
 python -m utils.validate_data --train_yaml example/7B.yaml
@@ -53,20 +57,25 @@ pip install "numpy<2.0"
 # check if has nvidia drivers
 # dl deps first then this or else wheels for xformers gets stuck
 # https://documentation.ubuntu.com/server/how-to/graphics/install-nvidia-drivers/index.html
-    # sudo apt-get install -y ubuntu-drivers-common
-    # sudo ubuntu-drivers list --gpgpu
-    # sudo ubuntu-drivers install --gpgpu nvidia:535-server
-    # sudo apt install nvidia-cuda-toolkit # skipped this
-    # nvcc --version    # and this skipped
-    # sudo apt install nvidia-utils-535-server
-    # sudo reboot # reboot to apply changes, best if you rebooted from console
-    # nvidia-smi
+sudo apt-get install -y ubuntu-drivers-common
+sudo ubuntu-drivers list --gpgpu
+sudo ubuntu-drivers install --gpgpu nvidia:535-server
+# sudo apt install nvidia-cuda-toolkit # skipped this
+# nvcc --version    # and this skipped
+sudo apt install nvidia-utils-535-server
+sudo reboot # reboot to apply changes, best if you rebooted from console
+nvidia-smi
+# watch -n 5 nvidia-smi
 
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=0
 
 # start training
+cd $HOME/mistral-finetune
 torchrun --nproc-per-node 1 -m train example.yaml
 
 # 16gb vram g4dn.xlarge gives this OOM error when run torch:
 # torch.cuda.OutOfMemoryError: CUDA out of memory. Tried to allocate 514.00 MiB. GPU 0 has a total capacity of 14.58 GiB of which 479.56 MiB is free. Including non-PyTorch memory, this process has 14.11 GiB memory in use. Of the allocated memory 13.58 GiB is allocated by PyTorch, and 115.49 MiB is reserved by PyTorch but unallocated. If reserved but unallocated memory is large try setting PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True to avoid fragmentation.  See documentation for Memory Management  (https://pytorch.org/docs/stable/notes/cuda.html#environment-variables)
+
+
+# g4dn.2xlarge, 32gb, $0.752
---
+++ src/utils/unsloth.sh
@@ -0,0 +1,5 @@
+# https://colab.research.google.com/github/unslothai/notebooks/blob/main/nb/Mistral_v0.3_(7B)-Conversational.ipynb#scrollTo=GqwZAVbRtjWF
+
+# had everything set up with aws.sh before doing this and it worked
+
+pip install unsloth
